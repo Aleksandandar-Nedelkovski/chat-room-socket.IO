970d39214e735e59a1be3e56de99f046f15de27d
Alex Nedelkovski <ace_nedelkovski@hotmail.com>
2019-11-22 17:40:13 -0600

    added Client

---
+++ Server/Database/DataAccess/DataAccessBase.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { Model } from "../Types";
+
+export abstract class DataAccessBase<T extends mongoose.Document> {
+  private model: Model;
+  protected constructor(model: Model) {
+    this.model = model;
+  }
+
+  Add(item: T): Promise<boolean> {
+    return new Promise<boolean>((callback, error) => {
+      this.model.create(item, (err: unknown, result: T) => {
+        if (err) {
+          error(err);
+        }
+        callback(!result);
+      });
+    });
+  }
+
+  GetAll(conditions: unknown, fields: unknown): Promise<unknown[]> {
+    return new Promise<T[]>((callback, error) => {
+      this.model.find(conditions, fields, (err: unknown, result: T[]) => {
+        if (err) {
+          error(err);
+        }
+        if (result) {
+          callback(result);
+        }
+      });
+    });
+  }
+}
--- Server/server.ts
+++ Server/server.ts
@@ -1,7 +1,8 @@
 import socket = require('socket.io');
+import { MessageDataAccess } from "./Database/DataAccess/Message";
 
-// import appSocket from "socket.io";
 export class SocketServer {
+  constructor(private messageDataAccess: MessageDataAccess = new MessageDataAccess()) { }
   public Start() {
     this.OnStarted();
   }
@@ -10,8 +11,23 @@ export class SocketServer {
     this.OnConnect(appSocket);
   }
 
-
   private OnConnect(io: socket.Server) {
-    new SocketServer().Start()
+    io.on("connection", (socket: any) => {
+      let lastRoom: string = "";
+      socket.on("joinRoom", (room: string) => {
+        if (lastRoom !== "") {
+          socket.leave(lastRoom);
+        }
+        if (room !== "") {
+          socket.join(room);
+        }
+        this.messageDataAccess.GetAll({ room: room }, { messageText: 1, _id: 0 }).then((msgs: string[]) => {
+          socket.emit('allMessages', msgs);
+        });
+        lastRoom = room;
+      })
+    })
   }
 }
+
+new SocketServer().Start()
