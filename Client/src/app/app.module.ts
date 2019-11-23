0d7ed611098e8b182c2ac411217ac180e145f08b
Alex Nedelkovski <ace_nedelkovski@hotmail.com>
2019-11-22 18:04:23 -0600

    set up bootstrap and nav bar

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
+import { NavigationComponent } from './components/navigation/navigation.component';
+
+const config: SocketIoConfig = {url: "http://localhost:3000", options: {}}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
